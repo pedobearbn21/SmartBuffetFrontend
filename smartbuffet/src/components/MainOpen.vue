d3c06e8f97fc7b53e9c2e940727179840fd807b9
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-02-05 23:19:35 +0700

    [Edited]
    fix life cycle problem that template update before data loading

--- smartbuffet/src/components/MainOpen.vue
+++ smartbuffet/src/components/MainOpen.vue
@@ -7,8 +7,8 @@
       <div class="border-2 border-gray-500   w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
         <div class="bg-white px-4 pt-4 pb-4 sm:p-6 sm:pb-4">
             <div class="flex justify-between "> <!-- กรอบของโต๊ะ -->
-              <div class=" p-2">NumTable{{name}}</div> <!--เลขโต๊ะ -->
-              <div class=" p-2 ">Status</div> <!-- สเตตัส -->
+              <div class=" p-2">NumTable: {{name}}</div> <!--เลขโต๊ะ -->
+              <div class=" p-2 ">Status: {{status}}</div> <!-- สเตตัส -->
             </div>
         </div>
       </div>
@@ -19,6 +19,6 @@
 <script>
   export default {
     name: 'MainOpen',
-    props: ['name']
+    props: ['name', 'status']
   }
 </script>
