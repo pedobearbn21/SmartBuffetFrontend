b0f1ecf169d9a33b1712c0c2780de22a052a1c50
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2021-02-14 21:56:41 +0700

    test

--- smartbuffet/src/App.vue
+++ smartbuffet/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div class="container w-screen"> -->
     <div class="">
-      <router-view />
+      <!-- <router-view /> -->
     </div>
   <!-- </div> -->
 </template>
@@ -12,7 +12,7 @@ import Layout from './components/Layout.vue'
 import MainOpen from './components/MainOpen.vue'
 import Opentable from './components/Opentable.vue'
 import AddMeat from './components/AddMeat.vue'
-import BillOrder from './components/AddMeat.vue'
+import BillOrder from './components/BillOrder.vue'
 import ListTable from './views/ListTable.vue'
 export default {
   name: 'App',
