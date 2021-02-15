26ad53fd67ff003298edf9adc85d2aea51cc7f19
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2021-02-15 22:58:26 +0700

    Cart
    update success

--- smartbuffet/src/App.vue
+++ smartbuffet/src/App.vue
@@ -1,34 +1,38 @@
 <template>
   <!-- <div class="container w-screen"> -->
     <div class="">
-      <!-- <ListTable/> -->
-      <router-view />
+      <Cart/>
+      <!-- <router-view /> -->
+      
     </div>
   <!-- </div> -->
 </template>
 
 <script>
-// import NavNew from './components/NavNew.vue'
-// import Layout from './components/Layout.vue'
-// import MainOpen from './components/MainOpen.vue'
-// import Opentable from './components/Opentable.vue'
-// import AddMeat from './components/AddMeat.vue'
-// import BillOrder from './components/BillOrder.vue'
-// import ListTable from './views/ListTable.vue'
-// import CardMeat from './components/CardMeat.vue'
-// import ListMenu from './views/ListMenu.vue'
+import NavNew from './components/NavNew.vue'
+import Layout from './components/Layout.vue'
+import MainOpen from './components/MainOpen.vue'
+import Opentable from './components/Opentable.vue'
+import AddMeat from './components/AddMeat.vue'
+import BillOrder from './components/BillOrder.vue'
+import ListTable from './views/ListTable.vue'
+import CardMeat from './components/CardMeat.vue'
+import Cart from './components/Cart.vue'
+import ListMenu from './views/ListMenu.vue'
 export default {
   name: 'App',
-  // components: {
-  //   NavNew,
-  //   Layout,
-  //   MainOpen,
-  //   Opentable,
-  //   AddMeat,
-  //   BillOrder,
-  //   ListTable,
-  //   CardMeat,
-  //   ListMenu,
-  // }
+  components: {
+    NavNew,
+    Layout,
+    MainOpen,
+    Opentable,
+    AddMeat,
+    BillOrder,
+    ListTable,
+    CardMeat,
+    Cart,
+    ListMenu,
+    ListTable
+  }
 }
 </script>
