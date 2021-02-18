02932fdd0ec6c3c1fe417a4a3748076545cae6ee
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-02-18 20:33:02 +0700

    [Merged]

--- smartbuffet/src/App.vue
+++ smartbuffet/src/App.vue
@@ -8,36 +8,36 @@
 </template>
 
 <script>
-import NavNew from './components/NavNew.vue'
-import Layout from './components/Layout.vue'
-import MainOpen from './components/MainOpen.vue'
-import Opentable from './components/Opentable.vue'
-import AddMeat from './components/AddMeat.vue'
-import BillOrder from './components/BillOrder.vue'
-import ListTable from './views/ListTable.vue'
-import CardMeat from './components/CardMeat.vue'
-import Cart from './components/Cart.vue'
-import OrderHistory from './components/OrderHistory.vue'
-import Bill from './components/Bill.vue'
-import SearchBar from './components/SearchBar.vue'
-import ListMenu from './views/ListMenu.vue'
+// import NavNew from './components/NavNew.vue'
+// import Layout from './components/Layout.vue'
+// import MainOpen from './components/MainOpen.vue'
+// import Opentable from './components/Opentable.vue'
+// import AddMeat from './components/AddMeat.vue'
+// import BillOrder from './components/BillOrder.vue'
+// import ListTable from './views/ListTable.vue'
+// import CardMeat from './components/CardMeat.vue'
+// import Cart from './components/Cart.vue'
+// import OrderHistory from './components/OrderHistory.vue'
+// import Bill from './components/Bill.vue'
+// import SearchBar from './components/SearchBar.vue'
+// import ListMenu from './views/ListMenu.vue'
 export default {
   name: 'App',
-  components: {
-    NavNew,
-    Layout,
-    MainOpen,
-    Opentable,
-    AddMeat,
-    BillOrder,
-    ListTable,
-    CardMeat,
-    Cart,
-    OrderHistory,
-    Bill,
-    SearchBar,
-    ListMenu,
-    ListTable
-  }
+  // components: {
+  //   NavNew,
+  //   Layout,
+  //   MainOpen,
+  //   Opentable,
+  //   AddMeat,
+  //   BillOrder,
+  //   ListTable,
+  //   CardMeat,
+  //   Cart,
+  //   OrderHistory,
+  //   Bill,
+  //   SearchBar,
+  //   ListMenu,
+  //   ListTable
+  // }
 }
 </script>
