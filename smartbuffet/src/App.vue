dbc4a0dad982691d24512e4de4cc25440e67bdce
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2021-02-24 15:27:26 +0700

    AddMeatOrder
    success eiei

--- smartbuffet/src/App.vue
+++ smartbuffet/src/App.vue
@@ -1,43 +1,43 @@
 <template>
-  <!-- <div class="container w-screen"> -->
+  <div class="">
     <div class="">
-      <!-- <Cart/> -->
-      <router-view />
+      <AddOrder/>
+     <!-- <router-view/> -->
     </div>
-  <!-- </div> -->
+  </div>
 </template>
 
 <script>
-// import NavNew from './components/NavNew.vue'
-// import Layout from './components/Layout.vue'
-// import MainOpen from './components/MainOpen.vue'
-// import Opentable from './components/Opentable.vue'
-// import AddMeat from './components/AddMeat.vue'
-// import BillOrder from './components/BillOrder.vue'
-// import ListTable from './views/ListTable.vue'
-// import CardMeat from './components/CardMeat.vue'
-// import Cart from './components/Cart.vue'
-// import OrderHistory from './components/OrderHistory.vue'
-// import Bill from './components/Bill.vue'
-// import SearchBar from './components/SearchBar.vue'
-// import ListMenu from './views/ListMenu.vue'
+import NavNew from './components/NavNew.vue'
+import Layout from './components/Layout.vue'
+import MainOpen from './components/MainOpen.vue'
+import Opentable from './components/Opentable.vue'
+import AddMeat from './components/AddMeat.vue'
+import BillOrder from './components/BillOrder.vue'
+import ListTable from './views/ListTable.vue'
+import CardMeat from './components/CardMeat.vue'
+import OrderHistory from './components/OrderHistory.vue'
+import Bill from './components/Bill.vue'
+import SearchBar from './components/SearchBar.vue'
+import ListMenu from './views/ListMenu.vue'
+import AddMeatOrder from './views/AddMeatOrder.vue'
 export default {
   name: 'App',
-  // components: {
-  //   NavNew,
-  //   Layout,
-  //   MainOpen,
-  //   Opentable,
-  //   AddMeat,
-  //   BillOrder,
-  //   ListTable,
-  //   CardMeat,
-  //   Cart,
-  //   OrderHistory,
-  //   Bill,
-  //   SearchBar,
-  //   ListMenu,
-  //   ListTable
-  // }
+   components: {
+     NavNew,
+     Layout,
+     MainOpen,
+     Opentable,
+     AddMeat,
+     BillOrder,
+     ListTable,
+     CardMeat,
+     OrderHistory,
+     Bill,
+     SearchBar,
+     ListMenu,
+     ListTable,
+     AddMeatOrder
+   }
 }
 </script>
