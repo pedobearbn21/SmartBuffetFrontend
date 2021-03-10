02fe9e353d88cb848afba8af37f60e9b4fb6cc6b
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-03-10 21:53:16 +0700

    [Edited] Changed Plate Color depends on status

--- smartbuffet/src/components/BillOrder.vue
+++ smartbuffet/src/components/BillOrder.vue
@@ -3,12 +3,12 @@
       <div class="max-w-md mx-auto h-auto bg-white rounded-b rounded-t" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
           <div class="grid grid-cols-12 flex items-stretch h-full font-bold">
             <div class="one col-span-1 " >
-              <div class="bg-red-500 rounded-l rounded-tl w-3.5 h-full"></div>
+              <div class="bg-yellow-300 rounded-l rounded-tl w-3.5 h-full" :class="status === 'ORDERED'?'bg-yellow-300':'bg-green-500'"></div>
             </div>
             <div class="two col-span-4 self-center">ออเดอร์ที่ {{  index  }}</div>
             <div class="three col-span-5 self-center">{{ status }}</div>
             <div class="four col-span-2 self-center h-auto px-2 py-2.5" >
-              <img src="../assets/eye.png" width="35rem">
+              <img src="../assets/eye.png" >
             </div>
           </div>
       </div>
--- smartbuffet/src/views/OrdersHistory.vue
+++ smartbuffet/src/views/OrdersHistory.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="justify-self-center mx-6 mt-5">
-        <div v-for="(item,index) in list" :key="item.id">
-            <BillOrder :index='index' :name='item.name' :status='item.status' :meat_list='item.meat_list' />
+    <div>
+        <div class="justify-self-center mx-6 mt-5   ">
+            <div v-for="(item,index) in list" :key="item.id">
+                <BillOrder :index='index' :name='item.name' :status='item.status' :meat_list='item.meat_list' />
+            </div>
+        </div>
+        <div>
+            <button type="submit" class="fixed bottom-0 left-0  h-12 px-2 w-full  border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-300 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              เช็คบิล
+            </button>
         </div>
     </div>
 </template>
