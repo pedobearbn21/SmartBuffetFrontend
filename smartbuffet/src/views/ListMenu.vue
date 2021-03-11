6ba4245282ed8661c7e78823bd6c7a3a0e54b575
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-03-11 13:27:10 +0700

    [Mock] Create name for testing

--- smartbuffet/src/views/ListMenu.vue
+++ smartbuffet/src/views/ListMenu.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="">
         <div v-for="item in list" :key='item.id' >
+            <div name='cardmeat-${id}'>
                 <CardMeat :id='item.id' :name='item.name' :quantity="item.quantity" :meat_img='item.meat_img' @ClickEvent="(e) => eventMeat(e)"/>
+            </div>
         </div>
         <footer class="pt-20"></footer>
             <button type="button" @click="navigateToOrder" class="w-full items-center flex text-white font-bold bg-green-200 md:bg-gray-300 px-2 text-center fixed bottom-0 h-16 ">
